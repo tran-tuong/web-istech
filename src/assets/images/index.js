39977c231e6051a190acfdc338fe796cf436553f
apollo <baotrung06092003@gmail.com>
2023-04-11 01:18:40 +0700

    Config file images

---
+++ src/assets/images/index.js
@@ -0,0 +1,11 @@
+const images = {
+    logo: require('../images/logo.png'),
+    readyToInspired: require('../images/ready-to.png'),
+    backgroundBoD: require('../images/background-BoD.png'),
+    backgroundMainPage: require('../images/background-main-page.png'),
+    backgroundMainPage1: require('../images/background1-main-page.png'),
+    countDownImg: require('../images/img1-main-page.png'),
+    sliderImg: require('../images/img2-main-page.png'),
+}
+
+export default images;
