fae3f77e06ac924ce42ef4d293a32bc953b402f3
HoangDat0412 <hoang2811dat@gmail.com>
2023-04-25 15:17:33 +0700

    d

--- src/pages/Department/Department.js
+++ src/pages/Department/Department.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
-
+import images from "../../assets/images";
+import "./Department.scss"
 export default function Department() {
     // const param = useParams();
 
@@ -8,7 +9,22 @@ export default function Department() {
     // const id = param.id 
   return (
     <div className='container'>
-        Department
+        <section className='Department_Head' style={{
+            paddingTop:"70px"
+        }}>
+            <h3> <span className='pe-4'>{"<"}</span>Department </h3>
+            <div className='d-flex justify-content-center align-items-center'>
+                <div className='row Department_Head_Content' style={{width:"90%"}}>
+                    <div className='col-4 Department_Head_Content_Right'>
+                    <img className="img-fluid img_Head_Department" src={images.sliderImg}></img>
+                    </div>
+                    <div className='col-8 Department_Head_Content_left'>
+                        <h1 className=''>Full Stack Dev</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
      
     </div>
   )
