a96f24765c13e4d601fdf0e351cb9d7e9f1dcb2f
HoangDat0412 <hoang2811dat@gmail.com>
2023-04-25 16:00:09 +0700

    sjda

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import HomeTemplate from "./templates/HomeTemplate";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import BoardOfDirectors from "./pages/BoardOfDirectors/BoardOfDirectors";
-import Department from "./pages/Department/Department";
-
+// import Department from "./pages/Department/Department";
+import FullStack from "./pages/FullStack/FullStack";
 
 function App() {
   return (
@@ -17,8 +17,8 @@ function App() {
               <Route index path="/home" element={<Home/>}/>
               <Route path="/about" element={<About/>} />
               <Route path="/board-of-director" element={<BoardOfDirectors/>} />
-              <Route path="/department">
-                <Route path="" element={<Department/>}/>
+              <Route path="/departments">
+                <Route path="fullstack" element={<FullStack/>}/>
               </Route>
           </Route>
 
