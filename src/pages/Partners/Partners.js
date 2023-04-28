dde6d334afba57f2920677369c301427238d1d89
tran-tuong <tranvantuong2k3@gmail.com>
2023-04-28 18:54:28 +0700

    test-tam

---
+++ src/pages/Partners/Partners.js
@@ -0,0 +1,56 @@
+import React, { createContext } from 'react'
+import './Partners.scss';
+import { data } from 'jquery';
+import Circle from '../../components/Circle';
+
+export const DataContext = createContext();
+
+export default function Partners() {
+    data = [
+      {
+        name: "Partner 1",
+        image: "https://we25.vn/media2018/Img_News/2022/05/10/tommy-shelby-cuoi-cung-co-nen-chet-trong-peaky-blinders_20220510084617.jpeg"
+      },
+      {
+        name: "Partner 2",
+        image: "https://we25.vn/media2018/Img_News/2022/05/10/tommy-shelby-cuoi-cung-co-nen-chet-trong-peaky-blinders_20220510084617.jpeg"
+      },
+      {
+        name: "Partner 3",
+        image: "https://we25.vn/media2018/Img_News/2022/05/10/tommy-shelby-cuoi-cung-co-nen-chet-trong-peaky-blinders_20220510084617.jpeg"
+      },
+      {
+        name: "Partner 4",
+        image: "https://we25.vn/media2018/Img_News/2022/05/10/tommy-shelby-cuoi-cung-co-nen-chet-trong-peaky-blinders_20220510084617.jpeg"
+      },
+      {
+        name: "Partner 5",
+        image: "https://we25.vn/media2018/Img_News/2022/05/10/tommy-shelby-cuoi-cung-co-nen-chet-trong-peaky-blinders_20220510084617.jpeg"
+      },
+      {
+        name: "Partner 6",
+        image: "https://we25.vn/media2018/Img_News/2022/05/10/tommy-shelby-cuoi-cung-co-nen-chet-trong-peaky-blinders_20220510084617.jpeg"
+      },
+    ];
+
+    const renderData = () => {
+      return data.map((item, index) => (
+        <Circle data={item} border partner key={index} />
+      ))
+    }
+    
+    return (
+      <div className='container department'>
+          <div className='department-header'>
+            <div className='content-heading'>
+              <h1>Partners</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+            </div>
+          </div>
+          
+          <div className='department-list'>
+              {renderData()}
+          </div>
+      </div>
+    )
+}
