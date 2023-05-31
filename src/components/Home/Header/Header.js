c80069389ca145d3c98a2002bbe9b64492160277
apollo <baotrung06092003@gmail.com>
2023-06-01 01:15:47 +0700

    minor changes

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -11,123 +11,134 @@ export default function Header() {
       if (event.currentTarget.className) setCheck(false);
   };
 
+  const handleChecked = () => {
+    setCheck(!check)
+    if (!check) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  }
+
   return (
-    <header className="header">
-      <nav className="navbar navbar-expand-lg">
-        <div className="container d-flex justify-content-between position-relative">
-          <input type="checkbox" onChange={() => setCheck(!check)} checked={check} className="toggle-menu"/>
-          <a className="navbar-brand position-relative" href="/">
-            <img src={images.logo} alt="Logo" className="navbar-logo" />
-          </a>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <div className="background-blur"></div>
-            <ul className="navbar-nav menu">
-              <li className="nav-item dropdown">
-                <div
-                  className="nav-link dropdown-toggle"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  About
-                </div>
-                <ul className="dropdown-menu" onClick={getClass} aria-labelledby="navbarDropdown">
-                  <li>
-                    <NavLink className="dropdown-item pt-3 pb-3" to='/about'>
-                      About Us
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink className="dropdown-item pt-3 pb-3" to='/board-of-director'>
-                      Board of Directors
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink className="dropdown-item pt-3 pb-3" to='/departments'>
-                      Departments
-                    </NavLink>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item dropdown">
-                <div
-                  className="nav-link dropdown-toggle"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Activities
-                </div>
-                <ul className="dropdown-menu" onClick={getClass} aria-labelledby="navbarDropdown">
-                  <li>
-                    <NavLink className="dropdown-item pt-3 pb-3" to="/events">
-                      Events
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink className="dropdown-item pt-3 pb-3" to="/internal-activites">
-                      Internal Activities
-                    </NavLink>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item dropdown">
-                <div
-                  className="nav-link dropdown-toggle"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Resources
-                </div>
-                <ul className="dropdown-menu" onClick={getClass} aria-labelledby="navbarDropdown">
-                  <li>
-                    <NavLink className="dropdown-item pt-3 pb-3" to="/blog">
-                      Blog
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink className="dropdown-item pt-3 pb-3" to="/resource-hub">
-                      Resource Hub
-                    </NavLink>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item dropdown">
-                <div
-                  className="nav-link dropdown-toggle"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Partners & Contacts
-                </div>
-                <ul className="dropdown-menu" onClick={getClass} aria-labelledby="navbarDropdown">
-                  <li>
-                    <NavLink className="dropdown-item pt-3 pb-3" to="/partners">
-                      Partners
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink className="dropdown-item pt-3 pb-3" to="/contact">
-                      Contact Us
-                    </NavLink>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <Button className="" to="/login" target='_blank'>Sign In</Button>
-              </li>
-            </ul>
+    <>
+      <header className="header">
+        <nav className="navbar navbar-expand-lg">
+          <div className="container d-flex justify-content-between position-relative">
+            <input type="checkbox" onChange={handleChecked} checked={check} className="toggle-menu"/>
+            <a className="navbar-brand position-relative" href="/">
+              <img src={images.logo} alt="Logo" className="navbar-logo" />
+            </a>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <div className="background-blur" onClick={handleChecked}></div>
+              <ul className="navbar-nav menu">
+                <li className="nav-item dropdown">
+                  <div
+                    className="nav-link dropdown-toggle"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    About
+                  </div>
+                  <ul className="dropdown-menu" onClick={getClass} aria-labelledby="navbarDropdown">
+                    <li>
+                      <NavLink className="dropdown-item pt-3 pb-3" to='/about'>
+                        About Us
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink className="dropdown-item pt-3 pb-3" to='/board-of-director'>
+                        Board of Directors
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink className="dropdown-item pt-3 pb-3" to='/departments'>
+                        Departments
+                      </NavLink>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item dropdown">
+                  <div
+                    className="nav-link dropdown-toggle"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Activities
+                  </div>
+                  <ul className="dropdown-menu" onClick={getClass} aria-labelledby="navbarDropdown">
+                    <li>
+                      <NavLink className="dropdown-item pt-3 pb-3" to="/events">
+                        Events
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink className="dropdown-item pt-3 pb-3" to="/internal-activites">
+                        Internal Activities
+                      </NavLink>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item dropdown">
+                  <div
+                    className="nav-link dropdown-toggle"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Resources
+                  </div>
+                  <ul className="dropdown-menu" onClick={getClass} aria-labelledby="navbarDropdown">
+                    <li>
+                      <NavLink className="dropdown-item pt-3 pb-3" to="/blog">
+                        Blog
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink className="dropdown-item pt-3 pb-3" to="/resource-hub">
+                        Resource Hub
+                      </NavLink>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item dropdown">
+                  <div
+                    className="nav-link dropdown-toggle"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Partners & Contacts
+                  </div>
+                  <ul className="dropdown-menu" onClick={getClass} aria-labelledby="navbarDropdown">
+                    <li>
+                      <NavLink className="dropdown-item pt-3 pb-3" to="/partners">
+                        Partners
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink className="dropdown-item pt-3 pb-3" to="/contact">
+                        Contact Us
+                      </NavLink>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Button className="" to="/login" target='_blank'>Sign In</Button>
+                </li>
+              </ul>
+            </div>
+            <Button className="btn-sign-in" to="/login" target='_blank'>Sign In</Button>
           </div>
-          <Button className="btn-sign-in" to="/login" target='_blank'>Sign In</Button>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+    </>
   );
 }
 
--- src/templates/HomeTemplate.js
+++ src/templates/HomeTemplate.js
@@ -8,7 +8,6 @@ export default function HomeTemplate() {
     <div>
         <Header/>
         <Outlet/>
-
         <Footer/>
     </div>
   )
