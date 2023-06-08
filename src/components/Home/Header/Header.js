a9793a9a599ebd02d8d46644ce7add3acb830f96
apollo <baotrung06092003@gmail.com>
2023-06-08 09:47:56 +0700

    delete header.scss

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import "./Header.scss";
 import images from "../../../assets/images";
 import Button from "../../Button";
 
