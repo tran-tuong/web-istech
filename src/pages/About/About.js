ef5612ab2afb3cc50ff0f18afe08e876694e768a
HoangDat0412 <hoang2811dat@gmail.com>
2023-09-16 20:58:12 +0700

    create about json

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./About.scss";
+import data from "../../Data/About.json";
 export default function About() {
   console.log(window.innerWidth);
+  console.log(data);
   return (
     <div className="about">
       <div className="container">
         <div class="row heading-div d-flex justify-content-center">
-          <div className="col-12 content-heading">
+          <div className="col-12 content-heading" data-aos="fade-up" data-aos-delay="70">
             <h1>About us</h1>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -18,108 +20,34 @@ export default function About() {
         </div>
       </div>
       <div>
-        <h2 className="text-center ab_value_heading">
+        <h2 className="text-center ab_value_heading" data-aos="fade-up"  data-aos-delay="150">
           Các <a className="ab_value">giá trị</a> mà chúng tôi theo đuổi
         </h2>
         <div className="container">
-          <div className="row">
-            <div className="col-4 ab-value-content">
-              <div className="ab_item_content">
-                <div className="d-flex justify-content-center flex-column align-items-center">
-                  <img src="https://uploads-ssl.webflow.com/612b7de9c64ea455b27fc10e/6310c97c52dc930bc2f07e8f_6.%20demonstrating%20integrity-p-500.png"></img>
-                  <h3 className="minisub-ab-heading">Thể hiện sự chính trực</h3>
-                  <p className="minisub-ab-content">
-                    AIESEC đề cao sự nhất quán và minh bạch trong mọi quyết định
-                    và hành động của mình. Chúng tôi nghiêm túc thực hiện các
-                    cam kết và làm việc đúng theo những quy tắc, thoả thuận được
-                    thống nhất bởi các thành viên.
-                  </p>
-                </div>
-              </div>
+          <div className="row" >
+         {data.map((item, index)=> {
+          return      <div className="col-4 ab-value-content" data-aos="fade-up" data-aos-delay="250">
+          <div className="ab_item_content">
+            <div className="d-flex justify-content-center flex-column align-items-center">
+              <img src={item.logo} alt={item.slogan}></img>
+              <h3 className="minisub-ab-heading">{item.slogan}</h3>
+              <p className="minisub-ab-content">
+                {item.content}
+              </p>
             </div>
+          </div>
+        </div>
+         })}
 
-            <div className="col-4 ab-value-content">
-              <div className="ab_item_content">
-                <div className="d-flex justify-content-center flex-column align-items-center">
-                  <img src="https://uploads-ssl.webflow.com/612b7de9c64ea455b27fc10e/6310c97c52dc930bc2f07e8f_6.%20demonstrating%20integrity-p-500.png"></img>
-                  <h3>Thể hiện sự chính trực</h3>
-                  <p>
-                    AIESEC đề cao sự nhất quán và minh bạch trong mọi quyết định
-                    và hành động của mình. Chúng tôi nghiêm túc thực hiện các
-                    cam kết và làm việc đúng theo những quy tắc, thoả thuận được
-                    thống nhất bởi các thành viên.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-4 ab-value-content">
-              <div className="ab_item_content">
-                <div className="d-flex justify-content-center flex-column align-items-center">
-                  <img src="https://uploads-ssl.webflow.com/612b7de9c64ea455b27fc10e/6310c97c52dc930bc2f07e8f_6.%20demonstrating%20integrity-p-500.png"></img>
-                  <h3>Thể hiện sự chính trực</h3>
-                  <p>
-                    AIESEC đề cao sự nhất quán và minh bạch trong mọi quyết định
-                    và hành động của mình. Chúng tôi nghiêm túc thực hiện các
-                    cam kết và làm việc đúng theo những quy tắc, thoả thuận được
-                    thống nhất bởi các thành viên.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-4 ab-value-content">
-              <div className="ab_item_content">
-                <div className="d-flex justify-content-center flex-column align-items-center">
-                  <img src="https://uploads-ssl.webflow.com/612b7de9c64ea455b27fc10e/6310c97c52dc930bc2f07e8f_6.%20demonstrating%20integrity-p-500.png"></img>
-                  <h3>Thể hiện sự chính trực</h3>
-                  <p>
-                    AIESEC đề cao sự nhất quán và minh bạch trong mọi quyết định
-                    và hành động của mình. Chúng tôi nghiêm túc thực hiện các
-                    cam kết và làm việc đúng theo những quy tắc, thoả thuận được
-                    thống nhất bởi các thành viên.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-4 ab-value-content">
-              <div className="ab_item_content">
-                <div className="d-flex justify-content-center flex-column align-items-center">
-                  <img src="https://uploads-ssl.webflow.com/612b7de9c64ea455b27fc10e/6310c97c52dc930bc2f07e8f_6.%20demonstrating%20integrity-p-500.png"></img>
-                  <h3>Thể hiện sự chính trực</h3>
-                  <p>
-                    AIESEC đề cao sự nhất quán và minh bạch trong mọi quyết định
-                    và hành động của mình. Chúng tôi nghiêm túc thực hiện các
-                    cam kết và làm việc đúng theo những quy tắc, thoả thuận được
-                    thống nhất bởi các thành viên.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-4 ab-value-content">
-              <div className="ab_item_content">
-                <div className="d-flex justify-content-center flex-column align-items-center">
-                  <img src="https://uploads-ssl.webflow.com/612b7de9c64ea455b27fc10e/6310c97c52dc930bc2f07e8f_6.%20demonstrating%20integrity-p-500.png"></img>
-                  <h3>Thể hiện sự chính trực</h3>
-                  <p>
-                    AIESEC đề cao sự nhất quán và minh bạch trong mọi quyết định
-                    và hành động của mình. Chúng tôi nghiêm túc thực hiện các
-                    cam kết và làm việc đúng theo những quy tắc, thoả thuận được
-                    thống nhất bởi các thành viên.
-                  </p>
-                </div>
-              </div>
-            </div>
+           
           </div>
         </div>
       </div>
       <div className="ab-mission container">
-        <h2 className="text-center ab_value_heading">
+        <h2 className="text-center ab_value_heading" data-aos="fade-up" data-aos-delay="150">
           <a className="ab_value">Sứ mệnh</a> của chúng tôi
         </h2>
-        <div className="ab-mission-content">
+        <div className="ab-mission-content" data-aos="fade-up" data-aos-delay="200">
           <div className="row">
             <div className="col-1 fix-resolution">
               <div
